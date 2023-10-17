48fe1034d1d1fdfedc9554366e1a3c4f870a8333
Rafael Ribeiro <Rafaelribeiro12@live.com>
2023-10-17 17:31:52 -0300

    Instalado Tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter, Inter_Tight, Comic_Neue, Roboto_Condensed } from 'next/font/google'
+import { Inter, Roboto_Condensed } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-const tight = Inter_Tight({subsets: ['latin'] })
-const abha = Comic_Neue({subsets: ['latin'], weight: ['300']})
 const roboto = Roboto_Condensed({subsets: ['latin'], weight: ['300']})
 
 export const metadata: Metadata = {
