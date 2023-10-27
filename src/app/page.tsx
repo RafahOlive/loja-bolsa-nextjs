0bb5e79bf5bb14db29ea7b3e28ebb92ff04951eb
Rafael Ribeiro <Rafaelribeiro12@live.com>
2023-10-27 12:18:43 -0300

    Criado pagina de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import Logo from "../../public/example.svg"
+import Logo from "../../public/example.svg";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,10 +8,20 @@ export default function Home() {
       <div className="flex flex-col gap-8 pt-14 pl-14">
         <h5 className="text-base">Verão 2020</h5>
         <h1 className="text-6xl">Nova coleção</h1>
-        <h4>Nos sabemos que precisa de estilo, então clique e veja as variedades</h4>
+        <h4>
+          Nos sabemos que precisa de estilo, então clique e veja as variedades
+        </h4>
+        <Link href="/product">
+          <button
+            className="bg-sky-600 text-slate-50 rounded w-52 h-16
+        text-2xl"
+          >
+            Compre agora!
+          </button>
+        </Link>
       </div>
       <div className="relative left-32">
-        <Image alt='logo' src={Logo}/>
+        <Image alt="logo" src={Logo} />
       </div>
     </div>
   );
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import bolsa from "../../assets/bolsa.jpg";
+
 export default function Product() {
-    return (
-      <h1>Produtos</h1>
-    )
-  }
+  return (
+    <div className="flex gap-8 m-4">
+      <div className="rounded bg-sky-300 px-5 py-6">Filtro</div>
+      <div className="rounded bg-sky-300 px-5 py-6">
+        <div className="">
+          <span>Bolsas</span>
+          <Link href="/">
+            <Image src={bolsa} alt="/" width={500} height={500} />
+            <span>Bolsa elegante</span>
+            <br />
+            <span>R$500,00</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
